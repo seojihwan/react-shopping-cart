478415196ce557bb37729aea47cb3299ec99a739
seojihwan <seojihwan213@naver.com>
2021-06-02 17:57:13 +0900

    feat: 로딩 컴포넌트 구현

---
+++ src/Components/@shared/Loading/style.ts
@@ -0,0 +1,47 @@
+import styled, { keyframes } from "styled-components";
+import { FlexCenter } from "../../../SharedStyled/Flex";
+
+const loadingAnimation = keyframes`
+  from {
+    transform:scale(1,1)
+  }
+  50% {
+    transform:scale(1,1.8)
+  }
+  to {
+    transform:scale(1,1)
+  }
+`;
+
+interface ContainerProps {
+  bgColor?: string;
+}
+
+const Container = styled(FlexCenter("div"))<ContainerProps>`
+  div {
+    width: 10px;
+    height: 40px;
+    background-color: ${({ bgColor = "#000" }) => `${bgColor};`};
+  }
+
+  div:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const Left = styled.div`
+  animation: ${loadingAnimation} 1s infinite ease-in-out;
+  animation-delay: -0.16s;
+`;
+
+const Center = styled.div`
+  animation: ${loadingAnimation} 1s infinite ease-in-out;
+`;
+
+const Right = styled.div`
+  animation: ${loadingAnimation} 1s infinite ease-in-out;
+  animation-delay: 0.16s;
+`;
+
+export { Container, Left, Center, Right };
+export { ContainerProps };
